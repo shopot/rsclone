3348067f8b92f873da0b7ecb6e9be041a73c63ce
Sapronov Konstantin <ks.costas@gmail.com>
2023-01-30 21:07:06 +0300

    fix: change settings

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -17,9 +17,9 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'error',
-    '@typescript-eslint/explicit-function-return-type': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
   },
 };
