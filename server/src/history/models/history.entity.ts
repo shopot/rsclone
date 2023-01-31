487c895509c0bc9614339fe13e70cafaa18945db
Sapronov Konstantin <ks.costas@gmail.com>
2023-01-31 15:05:45 +0300

    feat: add model history

---
+++ server/src/history/models/history.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('history')
+export class History {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ length: 8, nullable: false, unique: true })
+  roomId: string;
+
+  // Player names with separator "#", example: "Alex@Demon#Olga"
+  @Column({ length: 255, default: '' })
+  players: string;
+
+  // Player name, example: "Alex"
+  @Column({ length: 32, default: '' })
+  winner: string;
+
+  @Column({ default: 0 })
+  duration: number;
+
+  @Column({ default: 0 })
+  rounds: number;
+
+  @Column({ default: () => 'unixepoch()' })
+  createdAt: number;
+}
