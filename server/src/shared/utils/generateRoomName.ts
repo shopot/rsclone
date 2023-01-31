204ea5d7c7015035150fced6a5e5a5db4c6dd5fb
Sergey Koksharov <gentoosiastic@gmail.com>
2023-01-31 14:53:31 +0300

    fix: move constants.ts to src/shared

--- server/src/shared/utils/generateRoomName.ts
+++ server/src/shared/utils/generateRoomName.ts
@@ -1,5 +1,5 @@
 import { customAlphabet } from 'nanoid';
-import { ROOM_NAME_LENGTH } from './constants';
+import { ROOM_NAME_LENGTH } from '../constants';
 
 const nanoid = customAlphabet(
   '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
