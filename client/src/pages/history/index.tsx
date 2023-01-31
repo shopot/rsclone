8307d7fb69f50569f9a6a2c4c63baaf1cacdf690
Sapronov Konstantin <ks.costas@gmail.com>
2023-01-31 09:38:07 +0300

    feat: add pages

---
+++ client/src/pages/history/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HistoryPage = () => {
+  return <div>History Page</div>;
+};
+
+export default HistoryPage;
---
+++ client/src/pages/rating/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RatingPage = () => {
+  return <div>Rating Page</div>;
+};
+
+export default RatingPage;
