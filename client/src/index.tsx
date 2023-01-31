5b9070373d6f4f7722578205c98fa0cb07ae5cbb
Sapronov Konstantin <ks.costas@gmail.com>
2023-01-31 09:39:25 +0300

    refactor: change App component

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,15 @@
+import App from './app';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './components/App';
-import './main.scss';
 
 const rootElement = document.querySelector<HTMLDivElement>('#app');
+
 if (!rootElement) {
   throw new Error('Root element #app was not found');
 }
+
 const root = createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
