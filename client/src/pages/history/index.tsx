6b1aad89261ec40c6a059dfa9e4ec1c06b741740
Sergey Koksharov <gentoosiastic@gmail.com>
2023-02-01 13:52:42 +0300

    feat: display table on History page

--- client/src/pages/history/index.tsx
+++ client/src/pages/history/index.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect } from 'react';
+import Table from 'rc-table';
+import listify from 'listify';
+import prettyMs from 'pretty-ms';
 import { useHistoryStore } from '../../store/historyStore';
 
 const HistoryPage = () => {
@@ -9,14 +12,38 @@ const HistoryPage = () => {
   }, []);
 
   console.log(results);
+  const columns = [
+    {
+      title: 'Players',
+      dataIndex: 'players',
+      render: (value: string[]) => <>{listify(value, { finalWord: '&' })}</>,
+    },
+    {
+      title: 'Loser',
+      dataIndex: 'loser',
+    },
+    {
+      title: 'Duration',
+      dataIndex: 'duration',
+      render: (value: number) => <>{prettyMs(value * 1000)}</>,
+    },
+    {
+      title: 'Rounds',
+      dataIndex: 'rounds',
+    },
+  ];
 
   return (
     <div>
-      {isOnline
-        ? results.map((res) => {
-            return `${res.players.join(' ')}`;
-          })
-        : 'Nothing'}
+      {isOnline ? (
+        <Table
+          columns={columns}
+          data={results}
+          rowKey="id"
+        />
+      ) : (
+        'Nothing'
+      )}
     </div>
   );
 };
