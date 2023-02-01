e801926544b5de7af6932867342b8da5c591eb24
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-01 18:10:57 +0300

    feat: implement class Player

--- server/src/shared/libs/Player.ts
+++ server/src/shared/libs/Player.ts
@@ -1,18 +1,59 @@
 import { Card } from './Card';
-import { TypePlayerStatus } from '../types/TypePlayerStatus';
+import { TypePlayerMember } from '../types/TypePlayerMember';
+import { TypePlayerStatus } from '../types';
+import { CardDto } from './CardDto';
 
 export class Player {
-  id; // socket.id
-  name;
+  socketId; // socket.id wtf string?
+  name: string;
   cards: Card[];
-  status;
-  isDefending;
+  memberStatus: TypePlayerMember;
+  playerStatus: TypePlayerStatus;
+  isDefender;
 
-  constructor(id: string, name: string, status: TypePlayerStatus = 'regular') {
-    this.id = id;
+  constructor(
+    socketId: string,
+    name: string,
+    memberStatus: TypePlayerMember = TypePlayerMember.Regular,
+  ) {
+    this.socketId = socketId;
     this.name = name;
     this.cards = [];
-    this.status = status;
-    this.isDefending = false;
+    this.memberStatus = memberStatus;
+    this.isDefender = false;
+  }
+
+  public getPlayerId() {
+    return this.name;
+  }
+
+  public getSocketId() {
+    return this.socketId;
+  }
+
+  public getCardsCount() {
+    return this.cards.length;
+  }
+
+  public addCards(cards: Card[]) {
+    return this.cards.concat(cards);
+  }
+
+  public getCards() {
+    return this.cards;
+  }
+
+  public setPlayerStatus(status: TypePlayerStatus) {
+    this.playerStatus = status;
+  }
+
+  public getPlayerStatus() {
+    return this.playerStatus;
+  }
+
+  public lostCard(cardDto: CardDto) {
+    this.cards = this.cards.filter(
+      (card) => card.rank !== cardDto.rank && card.suit !== cardDto.suit,
+    );
   }
 }
