78c978b23bd11ebd792f0d253fcfbf5165d07459
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-01 18:06:37 +0300

    refactor: rename constant

--- server/src/shared/constants/index.ts
+++ server/src/shared/constants/index.ts
@@ -3,4 +3,4 @@ export const MIN_ROOM_SIZE = 2;
 export const MAX_ROOM_SIZE = 4;
 export const STARTING_CARDS_NUMBER = 6;
 export const MAX_ATTACKS_ROUND_1 = 5;
-export const MAX_ATTACKS = 6;
+export const MAX_ATTACKER_ROUND_SLOT = 6;
