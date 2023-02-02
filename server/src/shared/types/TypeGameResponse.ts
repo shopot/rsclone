dec0883ed42f4d3ed5a61736973341251bcfebe2
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-02 19:01:07 +0300

    feat: add roomId

--- server/src/shared/types/TypeGameResponse.ts
+++ server/src/shared/types/TypeGameResponse.ts
@@ -2,6 +2,7 @@ import { CardDto } from '../dto';
 import { TypePlayerStatus } from './TypePlayerStatus';
 
 export type TypeGameResponse = {
+  roomId?: string;
   playerId?: string;
   card?: CardDto;
   playerStatus?: TypePlayerStatus;
