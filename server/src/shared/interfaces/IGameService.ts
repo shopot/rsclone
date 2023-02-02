2abcdfcffc8b9c5f9636c0a110be9ca4caf02cf6
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-02 18:59:50 +0300

    feat: add IGameService interface

---
+++ server/src/shared/interfaces/IGameService.ts
@@ -0,0 +1,14 @@
+import { TypeGameResponse } from '../types/TypeGameResponse';
+
+export interface IGameService {
+  setFromServerGameStart(payload?: TypeGameResponse): void;
+  setFromServerGameOver(payload?: TypeGameResponse): void;
+  setFromServerAttackerSetActive(payload?: TypeGameResponse): void;
+  setFromServerAttackerOpenSuccess(payload?: TypeGameResponse): void;
+  setFromServerAttackerOpenFail(payload?: TypeGameResponse): void;
+  setFromServerAttackerPass(payload?: TypeGameResponse): void;
+  setFromServerDefenderSetActive(payload?: TypeGameResponse): void;
+  setFromServerDefenderCloseSuccess(payload?: TypeGameResponse): void;
+  setFromServerDefenderCloseFail(payload?: TypeGameResponse): void;
+  setFromServerSendPlayerStatus(payload?: TypeGameResponse): void;
+}
