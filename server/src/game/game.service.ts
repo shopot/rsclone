251563d95c3bd8039ece2937542fd77d5922cb1b
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-02 22:32:39 +0300

    feat: implement setFromClientCreateGame

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { Player } from '../shared/libs/Player';
+import { TypePlayerMember, TypeGameResponse } from '../shared/types';
+import { generateRoomName } from '../shared/utils/generateRoomName';
+import { IGameService } from '../shared/interfaces';
+import { Room } from '../shared/libs/Room';
 import { GameReceiveDto } from './dto';
 
 @Injectable()
-export class GameService {
+export class GameService implements IGameService {
+  private rooms: Map<string, Room>;
+
+  constructor() {
+    this.rooms = new Map();
+  }
+
+  private getRoom(roomId: string) {
+    if (!this.rooms.has(roomId)) {
+      throw new Error(`Room #${roomId} not found.`);
+    }
+
+    return this.rooms.get(roomId);
+  }
+
+  async setFromClientCreateGame(data: GameReceiveDto) {
+    const hostPlayer = new Player('', data.playerId, TypePlayerMember.Host);
+
+    const roomId = generateRoomName();
+
+    const room = new Room(roomId, hostPlayer, this);
+
+    this.rooms.set(roomId, room);
+
+    console.log(room);
+    // Send to client message: Game is created
+  }
+
   async setFromClientCreatePlayer(data: GameReceiveDto) {
     console.log('setCreatePlayer', data);
   }
@@ -41,5 +73,54 @@ export class GameService {
 
   async setFromClientSettings(data: GameReceiveDto) {
     console.log('setSettings', data);
+  }
+
+  async setFromServerGameStart(payload?: TypeGameResponse): Promise<void> {
+    console.log('interface');
+  }
+  async setFromServerGameOver(payload?: TypeGameResponse): Promise<void> {
+    console.log('interface');
+  }
+  async setFromServerAttackerSetActive(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
+  }
+  async setFromServerAttackerOpenSuccess(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
+  }
+  async setFromServerAttackerOpenFail(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
+  }
+  async setFromServerAttackerPass(payload?: TypeGameResponse): Promise<void> {
+    console.log('interface');
+  }
+
+  async setFromServerDefenderSetActive(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
+  }
+
+  async setFromServerDefenderCloseSuccess(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
+  }
+
+  async setFromServerDefenderCloseFail(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
+  }
+
+  async setFromServerSendPlayerStatus(
+    payload?: TypeGameResponse,
+  ): Promise<void> {
+    console.log('interface');
   }
 }
