8a6697af764eee6f534874ee1066d47b7b051e96
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-03 20:02:09 +0300

    feat: add test for Players.next()

---
+++ server/src/shared/libs/Players.spec.ts
@@ -0,0 +1,39 @@
+import { expect, test } from '@jest/globals';
+import { TypePlayerStatus } from './../types/TypePlayerStatus';
+import { TypePlayerMember } from './../types/TypePlayerMember';
+import { Players } from './Players';
+import { Player } from './Player';
+
+const player1 = new Player(
+  'socket-0',
+  'Demon',
+  TypePlayerMember.Host,
+  TypePlayerStatus.InGame,
+);
+
+const player2 = new Player(
+  'socket-0',
+  'Alex',
+  TypePlayerMember.Regular,
+  TypePlayerStatus.YouWinner,
+);
+
+const player3 = new Player(
+  'socket-0',
+  'Candy',
+  TypePlayerMember.Regular,
+  TypePlayerStatus.InGame,
+);
+
+const player4 = new Player(
+  'socket-0',
+  'Rolando',
+  TypePlayerMember.Regular,
+  TypePlayerStatus.InGame,
+);
+
+const players = new Players([player1, player2, player3, player4]);
+
+test('Test deepCopy', () => {
+  expect(players.nextPlayerInGame(player1)).toEqual(player3);
+});
