e10a4ca3be950406c492e07347b7b8d2b43d8069
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-04 11:58:05 +0300

    feat: aff adapter

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SocketAdapter } from './gateway.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+  });
+
+  app.useWebSocketAdapter(new SocketAdapter(app));
 
   await app.listen(3000);
 
