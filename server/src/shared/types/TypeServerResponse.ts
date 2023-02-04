b82ddf35b6a5b95f6f22801ca7456199215b629f
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-04 13:23:22 +0300

    feat: add type socket

--- server/src/shared/types/TypeServerResponse.ts
+++ server/src/shared/types/TypeServerResponse.ts
@@ -1,3 +1,4 @@
+import { Socket } from 'socket.io';
 import { TypeRoomStatus } from './TypeRoomStatus';
 import { CardDto } from '../dto';
 import { TypePlayerStatus } from './TypePlayerStatus';
@@ -5,6 +6,7 @@ import { TypePlayerStatus } from './TypePlayerStatus';
 export type TypeServerResponse = {
   roomId: string;
   socketId?: string;
+  socket?: Socket;
   playerId?: string;
   card?: CardDto;
   playerStatus?: TypePlayerStatus;
