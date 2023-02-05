c40d17d74d503a831356d736fd52f69e2a1eecb0
Sergey Koksharov <gentoosiastic@gmail.com>
2023-02-05 16:24:51 +0300

    refactor: add TypeServerError from server to client

---
+++ client/src/shared/types/TypeServerError.ts
@@ -0,0 +1,7 @@
+export enum TypeServerError {
+  JoinRoomFailed = 'JoinRoomFailed',
+  CreateRoomFailed = 'CreateRoomFailed',
+  ChangeNextPlayerFailed = 'ChangeNextPlayerFailed',
+  SetOpenCardFailed = 'SetOpenCardFailed',
+  SetCloseCardFailed = 'SetCloseCardFailed',
+}
