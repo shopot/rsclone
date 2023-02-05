ccc55094e7b6d608422852f0dc5314187def73ec
Sergey Koksharov <gentoosiastic@gmail.com>
2023-02-05 15:02:35 +0300

    feat: use 10 hexadecimal digits for room ids

--- server/src/shared/utils/generateRoomName.ts
+++
@@ -1,8 +0,0 @@
-import { randomBytes } from 'node:crypto';
-import { ROOM_NAME_LENGTH } from '../constants';
-
-export const generateRoomName = (): string => {
-  return randomBytes(ROOM_NAME_LENGTH)
-    .toString('base64')
-    .slice(0, ROOM_NAME_LENGTH);
-};
