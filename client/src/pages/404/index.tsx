e817c9694468dbc62d002186ed46b8c736fb84f8
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-06 08:31:09 +0300

    feat: add navigate

--- client/src/pages/404/index.tsx
+++ client/src/pages/404/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TypeRoute } from '../../shared/types';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleGoHome = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate(TypeRoute.Home);
+  };
+
   return (
     <div className="container">
       <h1 className="heading">404 Page not found.</h1>
+      <div className="text-center">
+        <button
+          type="button"
+          className="btn"
+          onClick={handleGoHome}
+        >
+          Go Home
+        </button>
+      </div>
     </div>
   );
 };
