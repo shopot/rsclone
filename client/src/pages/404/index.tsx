d7b1d39c8bf0700fdc0ec3528fdf3defd95fe97e
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-06 08:05:11 +0300

    feat: add css class container

--- client/src/pages/404/index.tsx
+++ client/src/pages/404/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found Page</h1>
+    <div className="container">
+      <h1 className="heading">404 Page not found.</h1>
     </div>
   );
 };
