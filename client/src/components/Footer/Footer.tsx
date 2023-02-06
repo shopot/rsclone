fb3ed5f15f7d53386a0128695e754f577d351ff6
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-06 09:15:56 +0300

    feat: add Footer

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.m.scss';
+
+export const Footer = () => {
+  return (
+    <div className={`footer ${styles.footer}`}>
+      <div className="container">
+        <div className="text-center">Footer</div>
+      </div>
+    </div>
+  );
+};
--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Footer } from '../Footer';
 import { Header } from '../Header';
 
 export const Layout = () => {
@@ -7,6 +8,7 @@ export const Layout = () => {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
