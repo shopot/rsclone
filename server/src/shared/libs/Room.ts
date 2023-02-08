3f5851e64f322ae52639e73bfa7595e8351d6a38
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-08 20:10:04 +0300

    fix: get next attacker

--- server/src/shared/libs/Room.ts
+++ server/src/shared/libs/Room.ts
@@ -383,15 +383,16 @@ export class Room {
       return this.players.getPlayersInGame();
     }
 
-    const playersStart = this.players.getAll();
-
     const startIndex = this.players.getPlayerIndexBySocketId(
       this.round.getStartPlayerSocketId(),
     );
 
-    const playersEnd = playersStart.slice(startIndex);
+    const playersAll = this.players.getAll();
 
-    let players = [...playersStart.slice(0, startIndex), ...playersEnd];
+    let players = [
+      ...playersAll.slice(startIndex + 1),
+      ...playersAll.slice(0, startIndex),
+    ];
 
     if (this.lastDefender === this.activePlayer) {
       players = players.filter(
