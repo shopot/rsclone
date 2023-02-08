43456935dc79ebcd5cacd46ca10ec57c0055f58c
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-08 23:04:14 +0300

    refactor: clean code

--- server/src/shared/libs/Players.ts
+++ server/src/shared/libs/Players.ts
@@ -46,49 +46,6 @@ export class Players {
     return player;
   }
 
-  public next(fromPlayer: Player): Player | null {
-    const playersInGame = this.getPlayersInGame();
-
-    if (playersInGame.length < 2) {
-      // Something went wrong.
-      return null;
-    }
-
-    // When has two players
-    if (playersInGame.length === 2) {
-      return fromPlayer === playersInGame[0]
-        ? playersInGame[1]
-        : playersInGame[0];
-    }
-
-    const idx = this.players.findIndex((plr) => {
-      return plr.getSocketId() === fromPlayer.getSocketId();
-    });
-
-    if (idx === -1) {
-      // Player not found. Something went wrong.
-      return null;
-    }
-
-    let foundPlayer = null;
-    const endIndex = this.totalCount();
-    let counter = 0; // Max value is equal endIndex
-
-    for (let i = idx; counter < endIndex; counter += 1) {
-      i = i < endIndex - 1 ? i + 1 : 0;
-
-      if (
-        this.players[i].getPlayerStatus() === TypePlayerStatus.InGame &&
-        i !== idx
-      ) {
-        foundPlayer = this.players[i];
-        break;
-      }
-    }
-
-    return foundPlayer;
-  }
-
   public getPlayersAsDto(): TypePlayerDto[] {
     return this.players.map((player) => {
       return {
