fe3a79b06b436f2710e8c50dc7e986aa3b96adb0
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-09 11:17:34 +0300

    feat: add logo

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import styles from './styles.m.scss';
+import durakLogoPng from '../../assets/durak-logo-text.webp';
 
 const HomePage = () => {
   return (
     <img
-      src="../../assets/durak-logo-text.webp"
+      src={durakLogoPng}
       alt="DUSRAK Card Game"
     />
   );
