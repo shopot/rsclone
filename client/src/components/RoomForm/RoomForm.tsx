86e98a5c2e3f5c75b22cf3ddc89208f2ab85e600
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-10 21:06:49 +0300

    feat: implement RoomForm

---
+++ client/src/components/RoomForm/RoomForm.tsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react';
+import styles from './RoomForm.m.scss';
+
+export const RoomForm = ({ title, onSubmit, onCancel }: IRoomFormProps) => {
+  const [playerName, setPlayerName] = useState('');
+  const [oldGameUI, setOldGameUI] = useState(true);
+  const [isPlayerNameValid, setIsPlayerNameValid] = useState(false);
+
+  useEffect(() => {
+    if (validateName(playerName)) {
+      setIsPlayerNameValid(true);
+    } else {
+      setIsPlayerNameValid(false);
+    }
+  }, [playerName]);
+
+  const validateName = (playerName: string) => {
+    if (playerName.match(/^[a-zA-Z0-9а-яА-Я_]+$/) && playerName.length > 3) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleChangePlayerName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPlayerName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (validateName(playerName)) {
+      onSubmit(playerName, oldGameUI);
+    }
+  };
+
+  return (
+    <div className={styles.form}>
+      <div className={styles.fromBody}>
+        <h2 className="heading">{title}</h2>
+        <label
+          htmlFor="player-name"
+          className={styles.playerName}
+        >
+          Your nickname:
+          <input
+            value={playerName}
+            onChange={handleChangePlayerName}
+            id="player-name"
+            placeholder="Enter your Nickname"
+            type="text"
+          ></input>
+        </label>
+        <label className={styles.oldUI}>
+          Old UI:
+          <input
+            type="checkbox"
+            checked={oldGameUI}
+            onChange={() => setOldGameUI(!oldGameUI)}
+          />
+        </label>
+      </div>
+      <div className={styles.formFooter}>
+        <button
+          onClick={handleSubmit}
+          className="btn"
+          type="button"
+          disabled={!isPlayerNameValid}
+        >
+          Create
+        </button>
+        <button
+          onClick={onCancel}
+          className="btn btn-secondary"
+          type="button"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export declare interface IRoomFormProps {
+  title: string;
+  onSubmit(playerName: string, oldGameUI: boolean): void;
+  onCancel(): void;
+}
