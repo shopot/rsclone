0561fdc55c462df298799cd131eff1e2a4a379e0
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-11 15:45:38 +0300

    feat: Implement save rating and history

--- server/src/game/game.module.ts
+++ server/src/game/game.module.ts
@@ -1,10 +1,11 @@
-import { HistoryModule } from './../history/history.module';
 import { Module } from '@nestjs/common';
+import { RatingModule } from './../rating/rating.module';
+import { HistoryModule } from './../history/history.module';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
 
 @Module({
   providers: [GameService, GameGateway],
-  imports: [HistoryModule],
+  imports: [HistoryModule, RatingModule],
 })
 export class GameModule {}
