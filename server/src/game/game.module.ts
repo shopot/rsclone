4af8be7270375f14b3e73c8c845a4344304246d6
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-11 13:35:00 +0300

    feat: add export HistoryModule

--- server/src/game/game.module.ts
+++ server/src/game/game.module.ts
@@ -1,8 +1,10 @@
+import { HistoryModule } from './../history/history.module';
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
 
 @Module({
   providers: [GameService, GameGateway],
+  imports: [HistoryModule],
 })
 export class GameModule {}
