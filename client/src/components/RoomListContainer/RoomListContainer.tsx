ae6211dcfd5fbc358025c3fe2fe915a149065bb5
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-11 08:11:11 +0300

    refactor: replace test data

--- client/src/components/RoomListContainer/RoomListContainer.tsx
+++ client/src/components/RoomListContainer/RoomListContainer.tsx
@@ -2,40 +2,18 @@ import { useEffect } from 'react';
 import { socketIOService } from '../../shared/api/socketio';
 import { MAX_NUMBER_OF_PLAYERS } from '../../shared/constants';
 import { TypeRoomStatus, TypeSocketEvent } from '../../shared/types';
+import { useRoomsStore } from '../../store/roomsStore';
 import { RoomList } from './RoomList';
 
-const roomData = [
-  {
-    playersCount: 1,
-    roomId: '95a0a45358',
-    status: TypeRoomStatus.WaitingForPlayers,
-  },
-  {
-    playersCount: 4,
-    roomId: '95a0a45358',
-    status: TypeRoomStatus.GameInProgress,
-  },
-  {
-    playersCount: 1,
-    roomId: '95a0a45358',
-    status: TypeRoomStatus.WaitingForPlayers,
-  },
-  {
-    playersCount: 4,
-    roomId: '95a0a45358',
-    status: TypeRoomStatus.WaitingForPlayers,
-  },
-  {
-    playersCount: 2,
-    roomId: '95a0a45358',
-    status: TypeRoomStatus.WaitingForPlayers,
-  },
-];
-
 export const RoomListContainer = () => {
-  // const rooms = useRoomsStore((state) => state.rooms);
+  const rooms = useRoomsStore((state) => state.rooms);
 
-  const rooms = roomData
+  useEffect(() => {
+    // Get rooms list
+    socketIOService.emit(TypeSocketEvent.GameRooms, { data: {} });
+  }, []);
+
+  const roomsList = rooms
     .map((room) => {
       const { roomId, playersCount, status } = room;
 
@@ -51,10 +29,5 @@ export const RoomListContainer = () => {
     })
     .sort((a, b) => b.playersCount - a.playersCount);
 
-  useEffect(() => {
-    // Get rooms list
-    socketIOService.emit(TypeSocketEvent.GameRooms, { data: {} });
-  }, []);
-
-  return <RoomList rooms={rooms} />;
+  return <RoomList rooms={roomsList} />;
 };
