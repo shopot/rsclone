1848784a2ca7ecba10c1e39829ca50605f814c31
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-13 09:54:17 +0300

    refactor: move declaration in file

--- server/src/shared/types/TypeGameError.ts
+++ server/src/shared/types/TypeGameError.ts
@@ -1,6 +1,20 @@
-import { TypeGameErrorType } from './TypeGameErrorType';
+export enum TypeGameErrorType {
+  JoinRoomFailed = 'JoinRoomFailed',
+  LeaveRoomFailed = 'LeaveRoomFailed',
+  CreateRoomFailed = 'CreateRoomFailed',
+  NextPlayerFailed = 'NextPlayerFailed',
+  OpenCardFailed = 'OpenCardFailed',
+  CloseCardFailed = 'CloseCardFailed',
+  AttackerPassFailed = 'AttackerPassFailed',
+  DefenderPickupFailed = 'DefenderPickupFailed',
+  GameStartFailed = 'GameStartFailed',
+  GameRestartFailed = 'GameRestartFailed',
+  GameRoomOpenFailed = 'GameRoomOpenFailed',
+  GameRoomNotFound = 'GameRoomNotFound',
+  MessageSendFailed = 'MessageSendFailed',
+}
 
 export type TypeGameError = {
   type: TypeGameErrorType;
-  message: string;
+  message?: string;
 };
