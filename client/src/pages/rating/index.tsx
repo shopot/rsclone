8a6a0e06c67f30dcd49e570dc9f148d5a9384157
Sergey Koksharov <gentoosiastic@gmail.com>
2023-02-14 10:54:19 +0300

    refactor: format dates more traditional way

--- client/src/pages/rating/index.tsx
+++ client/src/pages/rating/index.tsx
@@ -26,7 +26,9 @@ const RatingPage = () => {
       title: 'Last time played',
       dataIndex: 'lastGameAt',
       // Note: SQLite stores Unix timestamp in seconds; new Date expects it in milliseconds
-      render: (value: number) => <>{new Date(value * 1000).toLocaleString().slice(0, 17)}</>,
+      render: (value: number) => (
+        <>{new Date(value * 1000).toLocaleString('en-GB', { hourCycle: 'h24' })}</>
+      ),
     },
   ];
 
