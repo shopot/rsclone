7b2c7f76140a2d0e756f484dcde4eef32c7d058f
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-15 15:01:43 +0300

    feat: add winston logger

---
+++ server/src/app/app-logger.config.ts
@@ -0,0 +1,62 @@
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
+
+const enum LoggingLevels {
+  Error = 'error',
+  Warn = 'warn',
+  Info = 'info',
+  Debug = 'debug',
+}
+
+const isDevelopmentMode =
+  (process.env.NODE_ENV || 'production') !== 'production' ? true : false;
+
+const appLoggerConfig: {
+  level: LoggingLevels;
+  transports: (
+    | winston.transports.FileTransportInstance
+    | winston.transports.ConsoleTransportInstance
+  )[];
+} = {
+  level: LoggingLevels.Info,
+  transports: [
+    new winston.transports.File({
+      level: LoggingLevels.Error,
+      maxsize: 5242880, // 5MB
+      maxFiles: 5,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.json(),
+      ),
+      filename: `${__dirname}/../logs/error.log`,
+    }),
+    new winston.transports.File({
+      maxFiles: 5,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.json(),
+      ),
+      filename: `${__dirname}/../logs/combined.log`,
+    }),
+  ],
+};
+
+if (isDevelopmentMode) {
+  appLoggerConfig.transports.push(
+    new winston.transports.Console({
+      level: LoggingLevels.Debug,
+      format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ level: true, all: true }),
+        nestWinstonModuleUtilities.format.nestLike('GameServer', {
+          colors: true,
+          prettyPrint: true,
+        }),
+      ),
+    }),
+  );
+}
+
+export default appLoggerConfig;
