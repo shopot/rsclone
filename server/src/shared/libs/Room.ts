c3bbf89684e43056d7a04015e4b6ffcb87a93dc5
Sergey Koksharov <gentoosiastic@gmail.com>
2023-02-16 18:13:57 +0300

    refactor: move check for last player higher in startNextRound()

--- server/src/shared/libs/Room.ts
+++ server/src/shared/libs/Room.ts
@@ -529,6 +529,16 @@ export class Room {
       }
     }
 
+    if (this.players.totalCountInGame() === 1) {
+      const lastPlayer = this.players.getPlayersInGame()[0];
+      lastPlayer.setPlayerStatus(TypePlayerStatus.YouLoser);
+      this.lastLoser = lastPlayer;
+      this.activePlayer = lastPlayer;
+      this.setGameIsOver();
+
+      return;
+    }
+
     // active player got no cards and won the game; choose another active player
     if (this.activePlayer.getPlayerStatus() === TypePlayerStatus.YouWinner) {
       this.activePlayer =
@@ -543,16 +553,6 @@ export class Room {
 
     // this.defender = this.getNextPlayer(); // Can returns error!
     this.defender = this.getNextPlayer(this.attacker);
-
-    if (this.players.totalCountInGame() === 1) {
-      const lastPlayer = this.players.getPlayersInGame()[0];
-      lastPlayer.setPlayerStatus(TypePlayerStatus.YouLoser);
-      this.lastLoser = lastPlayer;
-      this.activePlayer = lastPlayer;
-      this.setGameIsOver();
-
-      return;
-    }
 
     this.defender.setPlayerRole(TypePlayerRole.Defender);
 
