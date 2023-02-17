54bbc081137a124f7e34452e9abc85548c7a5a31
Sapronov Konstantin <ks.costas@gmail.com>
2023-02-17 08:27:16 +0300

    feat: add case2xNoCardsFromDealtAttackerWins

--- client/src/shared/tests/testNames.ts
+++ client/src/shared/tests/testNames.ts
@@ -1,6 +1,7 @@
 export const testNames = [
   'case2xLastBeatAttackerWins',
   'case2xLastRoundDrawWins',
+  'case2xNoCardsFromDealtAttackerWins',
   'case3xDefenderWithNoCards',
   'case3xAttackerWithNoCards',
 ];
--- server/src/test-factory/cases/index.ts
+++ server/src/test-factory/cases/index.ts
@@ -1,11 +1,13 @@
 import { case2xLastBeatAttackerWins } from './case-2x-last-beat-attacker-wins';
 import { case2xLastRoundDrawWins } from './case-2x-last-round-draw-wins';
+import { case2xNoCardsFromDealtAttackerWins } from './case-2x-no-cards-from-dealt-attacker-wins';
 import { case3xDefenderWithNoCards } from './case-3x-defender-with-no-cards';
 import { case3xAttackerWithNoCards } from './case-3x-attacker-with-no-cards';
 
 export const testCases = {
   case2xLastBeatAttackerWins,
   case2xLastRoundDrawWins,
+  case2xNoCardsFromDealtAttackerWins,
   case3xDefenderWithNoCards,
   case3xAttackerWithNoCards,
 };
